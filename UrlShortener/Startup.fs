9af7e0c814948c75598eaf2ed458c10240c0daca
Dejan Milicic <dejan.milicic@gmail.com>
2020-05-23 00:17:02 +0200

    upgrading to latest asp.net

--- UrlShortener/Startup.fs
+++ UrlShortener/Startup.fs
@@ -6,6 +6,7 @@ open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 open WebSharper.AspNetCore
+open Microsoft.Extensions.Hosting
 
 type Startup() =
 
@@ -16,7 +17,7 @@ type Startup() =
                 .AddCookie("WebSharper", fun options -> ())
         |> ignore
 
-    member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment, db: Database.Context) =
+    member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment, db: Database.Context) =
         if env.IsDevelopment() then app.UseDeveloperExceptionPage() |> ignore
 
         app.UseAuthentication()
